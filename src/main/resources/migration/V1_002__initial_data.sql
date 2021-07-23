708fff8ad32662dd212851a4d2b3e7e8e4c7fefc
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2021-07-24 01:31:03 +0300

    add flyway

--- src/main/resources/migration/V1_002__initial_data.sql
+++ src/main/resources/migration/V1_002__initial_data.sql
@@ -1,12 +1,3 @@
-insert into public.confirmation_id_table (id, uuid, due_date, user_id)
-values  (1, '328cad31-8e0d-4eea-ac43-3b57c52dcf64', '2021-07-22 00:20:03.184000', 37);
-
-insert into public.credential (id, id_users, login, password)
-values  (2, 2, 'hrom', '$2a$10$beBjYbOcNByKAk2g7ley5uvfMlFPUXUa61eyxDjhWqGMQTUCtICVy'),
-        (1, 5, 'vikasolnce', '$2a$10$lBmoBvy1b/H02Y98JzbUlObFI7fX4SnEEp4FOpQtLs061tm505lZS'),
-        (9, 14, 'tester', '$2a$10$0sFwXPVifpiha8Eqmowzp.vFuikhXYRkvKviXf5Rz2oLToSNv3TTi'),
-        (3, 3, 'ratmir_1', '$2a$10$0JI3lW2i4rQLhrS6ZGVQFukHaFiyAnw9eIciylbLgySO7JpzHZscy');
-
 insert into public.department (id, department_name)
 values  (1, 'QA engineer'),
         (2, 'Software engineer'),
@@ -42,6 +33,14 @@ values  (14, 'Ivan', 'Ivanov', 'tester@bsuir.by', '1985-03-25', 2, '2021-06-23 2
         (2, 'Александр', 'Ахрамович', 'al-hrom-3@mail.ru', '1983-09-29', 1, '2021-05-27 23:15:12.000000', '2021-05-27 23:15:17.000000', false, 4, 'Владимирович', 3, true),
         (37, 'Сергей', 'Прохоров', 'al-hrom-3@nnnnmail.ru', '2000-04-03', 2, '2021-07-21 00:26:42.759000', '2021-07-21 00:26:42.759000', false, 4, 'Вл', 2, false);
 
+
+insert into public.credential (id, id_users, login, password)
+values  (2, 2, 'hrom', '$2a$10$beBjYbOcNByKAk2g7ley5uvfMlFPUXUa61eyxDjhWqGMQTUCtICVy'),
+        (1, 5, 'vikasolnce', '$2a$10$lBmoBvy1b/H02Y98JzbUlObFI7fX4SnEEp4FOpQtLs061tm505lZS'),
+        (9, 14, 'tester', '$2a$10$0sFwXPVifpiha8Eqmowzp.vFuikhXYRkvKviXf5Rz2oLToSNv3TTi'),
+        (3, 3, 'ratmir_1', '$2a$10$0JI3lW2i4rQLhrS6ZGVQFukHaFiyAnw9eIciylbLgySO7JpzHZscy');
+
+
 insert into public.users_role (id, id_user, id_role, permission)
 values  (2, 2, 1, null),
         (3, 3, 2, null),
@@ -51,3 +50,6 @@ values  (2, 2, 1, null),
         (8, 14, 1, null),
         (9, 14, 2, null),
         (10, 14, 3, null);
+
+insert into public.confirmation_id_table (id, uuid, due_date, user_id)
+values  (1, '328cad31-8e0d-4eea-ac43-3b57c52dcf64', '2021-07-22 00:20:03.184000', 37);
