13869fc4f09556bff6e4e0a77e495bd68b781449
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2021-08-03 00:12:50 +0300

    add methods to IDepartmentDataRepository

--- src/main/java/by/bsuir/repository/springdata/IDepartmentDataRepository.java
+++ src/main/java/by/bsuir/repository/springdata/IDepartmentDataRepository.java
@@ -4,11 +4,14 @@ import by.bsuir.domain.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IDepartmentDataRepository extends
         CrudRepository<Department, Integer>,
         JpaRepository<Department, Integer> {
 
-    Optional<Department> findDepartmentByDepartmentName(String departmentName);
+    List<Department> findByDepartmentNameContainingIgnoreCase(String name);
+
+    Optional<Department> findByDepartmentNameIgnoreCase(String name);
 }
