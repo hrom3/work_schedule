fb2d7c4c7053190796d5c8b4286d71f940a5cf92
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2021-08-05 00:12:30 +0300

    update swagger info

--- src/main/java/by/bsuir/beans/SwaggerConfig.java
+++ src/main/java/by/bsuir/beans/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package by.bsuir.beans;
 
+import by.bsuir.SpringBootStarter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,9 +8,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -19,9 +24,23 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                //.apis(RequestHandlerSelectors.any())
+                //.apis(RequestHandlerSelectors.basePackage("by.bsuir.controller"))
+                .apis(RequestHandlerSelectors.basePackage("by.bsuir"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        //String version = SpringBootStarter.class.getPackage().getImplementationVersion();
+        return new ApiInfo(
+                "Work schedule REST API",
+                "Some custom description of work schedule API.",
+                "0.1.20210723",
+                null,
+                new Contact("Aleksandr Ahramovich", "www.emc.bsuir.by", "al-hrom-3@mail.ru"),
+                "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0.html", Collections.emptyList());
     }
 
     @Override
