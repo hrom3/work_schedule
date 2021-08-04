e61aaa77a5474cbbe2ec996c5912817cccbed675
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2021-08-04 21:19:54 +0300

    refactor

--- src/main/java/by/bsuir/repository/springdata/IConfirmationDataRepository.java
+++ src/main/java/by/bsuir/repository/springdata/IConfirmationDataRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-
 public interface IConfirmationDataRepository
         extends CrudRepository<ConfirmationData, Long>,
         PagingAndSortingRepository<ConfirmationData, Long>,
--- src/main/java/by/bsuir/repository/springdata/IRoleDataRepository.java
+++ src/main/java/by/bsuir/repository/springdata/IRoleDataRepository.java
@@ -1,11 +1,8 @@
 package by.bsuir.repository.springdata;
 
-import by.bsuir.domain.Rate;
 import by.bsuir.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface IRoleDataRepository extends
         CrudRepository<Role, Integer>,
