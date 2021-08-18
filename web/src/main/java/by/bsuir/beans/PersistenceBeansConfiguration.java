7cca453b125a714fc7daf297f5afff180453f7fb
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2021-08-18 23:49:40 +0300

    remove SessionFactory

--- web/src/main/java/by/bsuir/beans/PersistenceBeansConfiguration.java
+++ web/src/main/java/by/bsuir/beans/PersistenceBeansConfiguration.java
@@ -1,10 +1,7 @@
 package by.bsuir.beans;
 
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -14,28 +11,6 @@ import java.util.Properties;
 
 public class PersistenceBeansConfiguration {
 
-    @Bean(name = "sessionFactory")
-    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception {
-        // Fix Postgres JPA Error:
-        // Method org.postgresql.jdbc.PgConnection.createClob() is not yet implemented.
-        // properties.put("hibernate.temp.use_jdbc_metadata_defaults",false);
-
-        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-
-        // Package contain entity classes
-        factoryBean.setPackagesToScan("by.bsuir");
-//        factoryBean.setMappingResources("hibernate"); // for xml config
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setHibernateProperties(getAdditionalProperties());
-        factoryBean.afterPropertiesSet();
-        //
-        SessionFactory sf = factoryBean.getObject();
-        System.out.println("## getSessionFactory: " + sf);
-        return sf;
-    }
-
-    //Entity Manager
-    @Primary
     @Autowired
     @Bean(name = "entityManagerFactory")
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
@@ -53,7 +28,6 @@ public class PersistenceBeansConfiguration {
 
     private Properties getAdditionalProperties() {
         Properties properties = new Properties();
-
 
         properties.put("hibernate.show_sql", "true");
         properties.put("current_session_context_class", "org.springframework.orm.hibernate5.SpringSessionContext");
